9af17bac90c32e38e351a544f26fe00be9b82c7a
ZuidVolt <czuid@icloud.com>
2025-06-25 13:20:19 +0800

    removed typeguard for now, as it just adds complexity that isn't necessary

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,3 @@
-from typing import TypeGuard
-
-type Uint = int
-
-
 def strip_dunder(s: str) -> str:
     """Remove leading and trailing double underscores from a string."""
     return s.strip("_")
-
-
-def is_uint(integer: int | Uint) -> TypeGuard[Uint]:
-    """Check if the given integer is a non-negative integer"""
-    return isinstance(integer, int | Uint) and integer > 0
