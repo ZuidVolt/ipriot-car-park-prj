762346efaf2237bafb29bddeefbc2d629249fd7d
ZuidVolt <czuid@icloud.com>
2025-06-25 09:28:16 +0800

    added a constructor and attributes to the car park class

--- src/car_park.py
+++ src/car_park.py
@@ -1,2 +1,20 @@
+from display import Display
+
+
 class CarPark:
-    pass
+    def __init__(
+        self,
+        location: str | None,
+        capacity: int,
+        plates: list[str] | None = None,
+        displays: list[Display] | None = None,
+    ) -> None:
+        self.location = location
+        self.capacity = capacity
+        self.plates = plates or []
+        self.displays = displays or []
+
+    def __str__(self) -> str:
+        return (
+            f"Car park at {self.location} with a capacity of {self.capacity}"
+        )
