faee83779ae72c2aaf4250c3e5dc1854906a8833
ZuidVolt <czuid@icloud.com>
2025-06-25 09:42:25 +0800

    Added constructors and attributes to the display and sensor classes

--- src/display.py
+++ src/display.py
@@ -1,2 +1,10 @@
 class Display:
-    pass
+    def __init__(
+        self, display_id: int, message: str = "", *, is_on: bool = False
+    ) -> None:
+        self.id = display_id
+        self.message = message
+        self.is_on = is_on
+
+    def __str__(self) -> str:
+        return f"Display {self.id}: {self.message}"
--- src/sensor.py
+++ src/sensor.py
@@ -1,2 +1,18 @@
 class Sensor:
+    def __init__(self, senor_id: int, car_park, *, is_active: bool) -> None:
+        self.senor_id = senor_id
+        self.car_park = car_park
+        self.is_active = is_active
+
+    def __str__(self) -> str:
+        status: str = ""
+        status = "is active" if self.is_active else "is not active"
+        return f"Display {self.senor_id}, Status: the senor {status}"
+
+
+class EntrySensor(Sensor):
+    pass
+
+
+class ExitSensor(Sensor):
     pass
