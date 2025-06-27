2426874364aeb12db186d4b503fecf34b6a6a5ea
ZuidVolt <czuid@icloud.com>
2025-06-27 12:19:19 +0800

    ref: refacted display class for each method to only have one responsibility

--- src/display.py
+++ src/display.py
@@ -35,20 +35,28 @@ class Display:
     def __str__(self) -> str:
         return f"Display {self.id}: {self.message}"
 
-    # im not sure if this is the best way (or correct way) to handle the update method,
-    # as it is both updating the message and logging the data (too many side effects and responsibilities?)
     def update(self, data: DisplayData) -> None:
         if "message" in data and data["message"] is not None:
             self.message = data["message"]
+        if "available_bays" in data:
+            self.available_bays = data["available_bays"]
+        if "temperature" in data:
+            self.temperature = data["temperature"]
+
+    def display(self, data: DisplayData) -> None:
         for key, val in data.items():
             display_val = round(val, 1) if isinstance(val, float) else val
             logger.info(f"{key}: {display_val}")
+
+    def update_and_display(self, data: DisplayData) -> None:
+        self.update(data)
+        self.display(data)
 
 
 if __name__ == "__main__":
     display = Display(1, "Welcome to the Car Park", is_on=True)
     print(display)
-    display.update({
+    display.update_and_display({
         "message": "Car Park Full",
         "available_bays": 10,
         "temperature": 22.55,
