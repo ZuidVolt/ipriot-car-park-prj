73f14b05f596319f9b60cfa6824424d6fec82406
ZuidVolt <czuid@icloud.com>
2025-06-27 14:24:57 +0800

    removed un-needed comments in main now

--- src/main.py
+++ src/main.py
@@ -5,15 +5,6 @@ from .config import Config
 from .display import Display
 from .sensor import EntrySensor, ExitSensor
 
-# TODO: create a car park object with the location moondalup, capacity 100, and log_file "moondalup.txt"
-# TODO: Write the car park configuration to a file called "moondalup_config.json"
-# TODO: Reinitialize the car park object from the "moondalup_config.json" file
-# TODO: create an entry sensor object with id 1, is_active True, and car_park car_park
-# TODO: create an exit sensor object with id 2, is_active True, and car_park car_park
-# TODO: create a display object with id 1, message "Welcome to Moondalup", is_on True, and car_park car_park
-# TODO: drive 10 cars into the car park (must be triggered via the sensor - NOT by calling car_park.add_car directly)
-# TODO: drive 2 cars out of the car park (must be triggered via the sensor - NOT by calling car_park.remove_car directly)
-
 
 def main() -> None:
     car_park = CarPark(
